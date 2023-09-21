6bbd94150e99c9b9c5da143049f854e555ce5f3d
joshitrejimon1 <joshit.rejimon@aurionpro.com>
2023-09-21 11:45:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using SimpleFactoryLibrary;
+using SimpleFactoryLibrary.Model;
+
+namespace SimpleCarFactoryConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoFactory factory = new AutoFactory();
+            var car = factory.MakeCar(AutoType.BMW);
+
+            car.Start();
+            car.Stop();
+
+        }
+    }
+}
